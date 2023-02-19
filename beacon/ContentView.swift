afb2d819ca8e7d33daba94f4d8e0b4d5d3e7deab
Gavin Mathes <gavinmathes@gmail.com>
2023-02-19 12:25:47 -0600

    search field stash

--- beacon/ContentView.swift
+++ beacon/ContentView.swift
@@ -1,43 +1,74 @@
-//
-//  ContentView.swift
-//  beacon
-//
-//  Created by Gavin Mathes on 1/8/23.
-//
-
 import SwiftUI
-import RealityKit
+import MapKit
 
-struct ContentView : View {
+struct ContentView: View {
+    @State private var address = ""
+    @State private var coordinate: CLLocationCoordinate2D?
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        VStack {
+            TextField("Enter address", text: $address)
+                .padding()
+            
+            MapView(coordinate: $coordinate, address: address)
+                .frame(height: 300)
+                .padding()
+            
+            if let coordinate = coordinate {
+                Text("Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)")
+            }
+        }
     }
 }
 
-struct ARViewContainer: UIViewRepresentable {
+struct MapView: UIViewRepresentable {
+    @Binding var coordinate: CLLocationCoordinate2D?
+    let address: String
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { placemarks, error in
+            guard let placemark = placemarks?.first, error == nil else {
+                print("Error: \(String(describing: error))")
+                return
+            }
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = placemark.location!.coordinate
+            view.removeAnnotations(view.annotations)
+            view.addAnnotation(annotation)
+            
+            coordinate = annotation.coordinate
+        }
+    }
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
 }
 
-#if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-#endif
