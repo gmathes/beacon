eb0c20f141a44bf7fa284dfc4a2d1892bd7b5e1f
Gavin Mathes <gavinmathes@gmail.com>
2023-02-19 18:37:58 -0600

    movable pin view

--- beacon/ContentView.swift
+++ beacon/ContentView.swift
@@ -1,177 +1,118 @@
 import SwiftUI
-import Combine
 import MapKit
 
-class MapSearch : NSObject, ObservableObject {
-    @Published var locationResults : [MKLocalSearchCompletion] = []
-    @Published var searchTerm = ""
-    
-    private var cancellables : Set<AnyCancellable> = []
-    
-    private var searchCompleter = MKLocalSearchCompleter()
-    private var currentPromise : ((Result<[MKLocalSearchCompletion], Error>) -> Void)?
-    
-    override init() {
-        super.init()
-        searchCompleter.delegate = self
-        
-        $searchTerm
-            .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
-            .removeDuplicates()
-            .flatMap({ (currentSearchTerm) in
-                self.searchTermToResults(searchTerm: currentSearchTerm)
-            })
-            .sink(receiveCompletion: { (completion) in
-                //handle error
-            }, receiveValue: { (results) in
-                self.locationResults = results
-            })
-            .store(in: &cancellables)
-    }
-    
-    func searchTermToResults(searchTerm: String) -> Future<[MKLocalSearchCompletion], Error> {
-        Future { promise in
-            self.searchCompleter.queryFragment = searchTerm
-            self.currentPromise = promise
-        }
-    }
-}
-
-extension MapSearch : MKLocalSearchCompleterDelegate {
-    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-            currentPromise?(.success(completer.results))
-        }
-    
-    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-        //could deal with the error here, but beware that it will finish the Combine publisher stream
-        //currentPromise?(.failure(error))
-    }
-}
-
-class DetailViewModel : ObservableObject {
-    @Published var isLoading = true
-    @Published private var coordinate : CLLocationCoordinate2D?
-    @Published var region: MKCoordinateRegion = MKCoordinateRegion()
-    
-    var coordinateForMap : CLLocationCoordinate2D {
-        coordinate ?? CLLocationCoordinate2D()
-    }
-    
-    func reconcileLocation(location: MKLocalSearchCompletion) {
-        let searchRequest = MKLocalSearch.Request(completion: location)
-        let search = MKLocalSearch(request: searchRequest)
-        search.start { (response, error) in
-            if error == nil, let coordinate = response?.mapItems.first?.placemark.coordinate {
-                self.coordinate = coordinate
-                self.region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
-                self.isLoading = false
-            }
-        }
-    }
-    
-    func clear() {
-        isLoading = true
-    }
-}
-
-struct Detail : View {
-    var locationResult : MKLocalSearchCompletion
-    @StateObject private var viewModel = DetailViewModel()
-    
-    struct Marker: Identifiable {
-        let id = UUID()
-        var location: MapMarker
-    }
-    
-    var body: some View {
-        Group {
-            if viewModel.isLoading {
-                Text("Loading...")
-            } else {
-                Map(coordinateRegion: $viewModel.region,
-                    annotationItems: [Marker(location: MapMarker(coordinate: viewModel.coordinateForMap))]) { (marker) in
-                    marker.location
-                }
-            }
-        }.onAppear {
-            viewModel.reconcileLocation(location: locationResult)
-        }.onDisappear {
-            viewModel.clear()
-        }
-        .navigationTitle(Text(locationResult.title))
-    }
-}
-
-
 struct ContentView: View {
-    @StateObject private var mapSearch = MapSearch()
-    @State private var selectedLocation: MKLocalSearchCompletion?
+    @State private var coordinate = CLLocationCoordinate2D()
+    @State private var annotation = MKPointAnnotation()
+    @State private var showTextField = false
+    @State private var address = ""
+
     var body: some View {
-        NavigationView {
-            ZStack {
-                
-                VStack {
+        ZStack(alignment: .top) {
+            MapView(coordinate: $coordinate, annotation: $annotation)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                HStack {
                     Spacer()
-                    Form {
-                        Section {
-                            TextField("Address", text: $mapSearch.searchTerm)
-                        }
-                        Section {
-                            ForEach(mapSearch.locationResults, id: \.self) { location in
-                                Button(action: {
-                                    selectedLocation = location
-                                }) {
-                                    VStack(alignment: .leading) {
-                                        Text(location.title)
-                                        Text(location.subtitle)
-                                            .font(.system(.caption))
-                                    }
-                                }
-                            }
-                        }
+                    if showTextField {
+                        TextField("Enter a label", text: $address, onCommit: {
+                            self.showTextField = false
+                        })
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
                     }
-                    .background(Color.white.opacity(0.7))
-                    .frame(height: 400)
+                    
+                    VStack {
+                        Button(action: {
+                            self.showTextField.toggle()
+                        }) {
+                            Image(systemName: "keyboard.badge.eye")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        .padding()
+                        Button(action: {
+                            self.showTextField = true
+                        }) {
+                            Image(systemName: "camera.viewfinder")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        .padding()
+                    }
                 }
-                
-                if let coordinate = selectedLocation {
-                    Text("Place: \(coordinate.title)")
-                }
-                
-                MapView(location: selectedLocation)
-                    .frame(height: 400)
-                    .edgesIgnoringSafeArea(.all)
             }
         }
     }
 }
 
-struct MapView: View {
-    @StateObject private var viewModel = DetailViewModel()
-    var location: MKLocalSearchCompletion?
-    
-    struct Marker: Identifiable {
-        let id = UUID()
-        var location: MapMarker
+struct MapView: UIViewRepresentable {
+    @Binding var coordinate: CLLocationCoordinate2D
+    @Binding var annotation: MKPointAnnotation
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.addGestureRecognizer(UILongPressGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.mapLongPressed(gestureRecognizer:))))
+        return mapView
     }
-    
-    var body: some View {
-        Group {
-            if viewModel.isLoading {
-                Text("Loading...")
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        view.removeAnnotations(view.annotations)
+        view.addAnnotation(annotation)
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+
+        @objc func mapLongPressed(gestureRecognizer: UIGestureRecognizer) {
+            if gestureRecognizer.state == .began {
+                let mapView = gestureRecognizer.view as! MKMapView
+                let coordinate = mapView.convert(gestureRecognizer.location(in: mapView), toCoordinateFrom: mapView)
+                parent.coordinate = coordinate
+                parent.annotation.coordinate = coordinate
+            }
+        }
+
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "Annotation"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
             } else {
-                Map(coordinateRegion: $viewModel.region,
-                    annotationItems: [Marker(location: MapMarker(coordinate: viewModel.coordinateForMap))]) { (marker) in
-                    marker.location
-                }
+                annotationView?.annotation = annotation
             }
-        }.onAppear {
-            if let location = location {
-                viewModel.reconcileLocation(location: location)
+
+            return annotationView
+        }
+
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            if let annotation = view.annotation as? MKPointAnnotation {
+                parent.coordinate = annotation.coordinate
+                parent.annotation = annotation
             }
-        }.onDisappear {
-            viewModel.clear()
+        }
+
+        func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+            if let annotationView = views.first {
+                annotationView.setSelected(true, animated: true)
+            }
         }
     }
 }
-
